c37ac92dc60624c1d4b290e3d41fe87027139e1c
carterj7383 <156561727+carterj7383@users.noreply.github.com>
2024-09-16 17:00:12 +0000

    reverted hello.py

--- M2T1/hello.py
+++ M2T1/hello.py
@@ -1,35 +1,19 @@
-from flask import Flask, redirect, render_template, request, url_for
-from flask_sqlalchemy import SQLAlchemy
+# minimal Flask app
+from flask import Flask, render_template
 
+#print("Starting flask program from ", __name__)
 app = Flask(__name__)
-app.config["DEBUG"] = True
+# Do any app specific setup here
+# for instance, loading a database
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="the username from the 'Databases' tab",
-    password="the password you set on the 'Databases' tab",
-    hostname="the database host address from the 'Databases' tab",
-    databasename="the database name you chose, probably yourusername$comments",
-)
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
-
-class Comment(db.Model):
-
-    __tablename__ = "comments"
-
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(4096))
-
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    if request.method == "GET":
-        return render_template("main_page.html", comments=Comment.query.all())
+    # find the template in /templates
 
-    comment = Comment(content=request.form["contents"])
-    db.session.add(comment)
-    db.session.commit()
-    return redirect(url_for('index'))
+    return render_template("main_page.html", name=name)
+
+
+
+@app.route("/action")
+def action():
+    return "Hello from the action route!"
