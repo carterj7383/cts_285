a2012fe978c0cebafa092cee4f3a06d30caaeaef
carterj7383 <156561727+carterj7383@users.noreply.github.com>
2024-09-16 17:01:20 +0000

    hello.py update

--- M2T1/hello.py
+++ M2T1/hello.py
@@ -1,19 +1,14 @@
-# minimal Flask app
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
-#print("Starting flask program from ", __name__)
 app = Flask(__name__)
-# Do any app specific setup here
-# for instance, loading a database
+app.config["DEBUG"] = True
 
-@app.route("/")
+comments = []
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    # find the template in /templates
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
 
-    return render_template("main_page.html", name=name)
-
-
-
-@app.route("/action")
-def action():
-    return "Hello from the action route!"
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
